ff87cfd8a43e9b96c5f49ddf69a0bd40d28f20ee
doityoo <yitsky@naver.com>
2022-11-17 17:01:40 +0900

    feat: css inline 스타일 사용하여 동적 스타일 적용하기

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,34 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if (enteredValue.trim().length > 0) {
+      setIsValid(true);
+      // enterValue의 문자열 길이가 0보다 크면, isValid = true로 만들어서 원래 CSS 속성으로 돌려줄 것
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+      // trim()은 공백 제거 메서드 
+      // enteredValue의 문자열 길이가 0이라면 return으로 함수 실행 종료
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
+        <input style={{ borderColor: !isValid ? 'red' : 'black', backgroundColor: !isValid ? 'salmon' : 'transparent' }}
+          type="text"
+          onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
