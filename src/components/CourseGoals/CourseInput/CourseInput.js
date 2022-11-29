132b95d0a3a39a35c51da8f1f17575b76818bc95
doityoo <yitsky@naver.com>
2022-11-29 20:02:24 +0900

    feat: input 입력 후 공백으로 만들기

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -2,6 +2,45 @@ import React, { useState } from 'react';
 import styled from 'styled-components'
 import Button from '../../UI/Button/Button';
 
+const CourseInput = props => {
+  const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = event => {
+    if (enteredValue.trim().length > 0) {
+      setIsValid(true);
+      // enterValue의 문자열 길이가 0보다 크면, isValid = true로 만들어서 원래 CSS 속성으로 돌려줄 것
+    }
+    setEnteredValue(event.target.value);
+  };
+  
+  const formSubmitHandler = event => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+      // trim()은 공백 제거 메서드 
+      // enteredValue의 문자열 길이가 0이라면 return으로 함수 실행 종료
+    }
+    props.onAddGoal(enteredValue);
+    setEnteredValue("");
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      {/* <formControl className={!isValid && 'invalid'}> 이렇게 사용할 수 있고, 아래 코드처럼 props를 사용해서 동적으로 가능하다  */}
+      <FormControl invalid={!isValid}>
+        <label>Course Goal</label>
+        <input
+          value={enteredValue}
+          type="text"
+          onChange={goalInputChangeHandler} />
+      </FormControl>
+      <Button type="submit">Add Goal</Button> 
+    </form>
+  );
+};
+
 const FormControl = styled.div`
   margin: 0.5rem 0;
 
@@ -29,41 +68,6 @@ const FormControl = styled.div`
 }
 `
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
-  const [isValid, setIsValid] = useState(true);
-
-  const goalInputChangeHandler = event => {
-    if (enteredValue.trim().length > 0) {
-      setIsValid(true);
-      // enterValue의 문자열 길이가 0보다 크면, isValid = true로 만들어서 원래 CSS 속성으로 돌려줄 것
-    }
-    setEnteredValue(event.target.value);
-  };
-
-  const formSubmitHandler = event => {
-    event.preventDefault();
-    if (enteredValue.trim().length === 0) {
-      setIsValid(false);
-      return;
-      // trim()은 공백 제거 메서드 
-      // enteredValue의 문자열 길이가 0이라면 return으로 함수 실행 종료
-    }
-    props.onAddGoal(enteredValue);
-  };
-
-  return (
-    <form onSubmit={formSubmitHandler}>
-      {/* <formControl className={!isValid && 'invalid'}> 이렇게 사용할 수 있고, 아래 코드처럼 props를 사용해서 동적으로 가능하다  */}
-      <FormControl invalid={!isValid}>
-        <label>Course Goal</label>
-        <input
-          type="text"
-          onChange={goalInputChangeHandler} />
-      </FormControl>
-      <Button type="submit">Add Goal</Button> 
-    </form>
-  );
-};
-
 export default CourseInput;
+
+
